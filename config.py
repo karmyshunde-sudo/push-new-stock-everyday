325dda2382408abbc0d2a4624eea6609a98eecdb
karmyshunde-sudo <karmy.shunde@gmail.com>
2025-08-26 16:08:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ class Config:
     # 默认的企业微信Webhook（实际使用中会被环境变量覆盖）
     WECOM_WEBHOOK = ""
     
+    WECOM_MESFOOTER = "【消息由push-new-stock-everyday发送】"
+    
     # 日志配置
     LOG_LEVEL = "INFO"
     LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"

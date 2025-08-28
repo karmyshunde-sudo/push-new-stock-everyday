0850a7bb2d4f6b06384aab0caad107a76be98f44
karmyshunde-sudo <karmy.shunde@gmail.com>
2025-08-28 15:06:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     # 默认的企业微信Webhook（实际使用中会被环境变量覆盖）
     WECOM_WEBHOOK = ""
     
-    WECOM_MESFOOTER = "【消息由push-new-stock-everyday发送】"
+    WECOM_MESFOOTER = "【消息由GIT-push-new-stock-everyday发送】"
     
     # 日志配置
     LOG_LEVEL = "INFO"
